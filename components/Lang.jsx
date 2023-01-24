aa9d793c5ddb2e2dabbb9b0f99689e820ac03599
Felizxfell <felizxhgf@gmial.com>
2023-01-24 17:41:44 -0600

    fix merge pr

--- components/Lang.jsx
+++ components/Lang.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from 'next/link';
 
 import nextConfig from "../next-i18next.config";
